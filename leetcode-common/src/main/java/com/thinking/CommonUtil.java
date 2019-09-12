b523f96dea2124961839ef070a4bea7c35ecc247
thinking <thinking_fioa@163.com>
2019-09-12 15:48:08 +0800

    leetcode-array leetcode 11

--- leetcode-common/src/main/java/com/thinking/CommonUtil.java
+++ leetcode-common/src/main/java/com/thinking/CommonUtil.java
@@ -12,7 +12,14 @@ public class CommonUtil {
   /**
    * {@link List <Integer>}递增排序.
    */
-  public static void sortListAsc(Integer numArray) {
-    Collections.sort(Arrays.asList(numArray));
+  public static void sortListAsc(List<Integer> numList) {
+    Collections.sort(numList);
+  }
+
+  /**
+   * 对int[]数组排序
+   */
+  public static void sortArrayAsc(int[] nums) {
+    Arrays.sort(nums);
   }
 }
