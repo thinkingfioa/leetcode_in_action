352daad9dcf4dfdf79f2da6e53dfe388fee18503
thinking <thinking_fioa@163.com>
2019-10-03 08:19:27 +0800

    leetcode-array leetcode54

--- leetcode-array/src/main/java/com/thinking/medium/Leetcode48.java
+++ leetcode-array/src/main/java/com/thinking/medium/Leetcode48.java
@@ -5,9 +5,9 @@ package com.thinking.medium;
  *
  * 给定一个二维数组 matrix[N][N]代表一个Image，顺时针循转90度。空间复杂度为O(1)
  *
- * 思路: 顺时针旋转 == 先翻转，再斜对角线翻转
+ * 思路: 顺时针旋转 == 先行翻转，再斜对角线翻转。先行翻转，行的逆序。斜对角线翻转公式(i, j) <-> (n-1-j, n-1-i)
  *
- * * 参考Go语言项目.
+ * 参考Go语言项目.
  *
  * <pre>
  *  https://github.com/thinkingfioa/go-learning/tree/master/leetcode-in-action/src/leetcode-array
