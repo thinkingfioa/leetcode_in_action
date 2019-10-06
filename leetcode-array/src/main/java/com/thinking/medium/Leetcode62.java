8719de7b21898ede206a03a514fb72c933660cbb
thinking <thinking_fioa@163.com>
2019-10-06 17:13:47 +0800

    leetcode-array leetcode63

--- leetcode-array/src/main/java/com/thinking/medium/Leetcode62.java
+++ leetcode-array/src/main/java/com/thinking/medium/Leetcode62.java
@@ -20,20 +20,20 @@ package com.thinking.medium;
 public class Leetcode62 {
 
   public int uniquePaths(int m, int n) {
-    int[][] dp = new int[n + 1][m + 1];
-    for (int i = 1; i <= n; i++) {
-      dp[i][1] = 1;
+    int[][] dp = new int[n][m];
+    for (int i = 0; i < n; i++) {
+      dp[i][0] = 1;
     }
-    for (int i = 1; i <= m; i++) {
-      dp[1][i] = 1;
+    for (int i = 0; i < m; i++) {
+      dp[0][i] = 1;
     }
-    for (int i = 2; i <= n; i++) {
-      for (int j = 2; j <= m; j++) {
+    for (int i = 1; i < n; i++) {
+      for (int j = 1; j < m; j++) {
         dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
 
       }
     }
-    return dp[n][m];
+    return dp[n - 1][m - 1];
   }
 
   public static void main(String[] args) {
