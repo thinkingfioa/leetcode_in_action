4cb938e53d06444297a83843733790a2d9a97e0a
thinking <thinking_fioa@163.com>
2019-10-13 17:21:48 +0800

    leetcode-array leetcode66

---
+++ leetcode-array/src/main/java/com/thinking/easy/Leetcode66.java
@@ -0,0 +1,20 @@
+package com.thinking.easy;
+
+/**
+ * Title: Plus One
+ *
+ * 题目: 给定数组nums={4,3,2,1}，代表数字4321，计算 4321+1= {4,3,2,2}，以数组返回.
+ *
+ * 思路: 常规题目，从右往左利用数学加法公式，注意处理进位.
+ *
+ * 参考Go语言项目.
+ *
+ * <pre>
+ *  https://github.com/thinkingfioa/go-learning/tree/master/leetcode-in-action/src/leetcode-array
+ * </pre>
+ *
+ * @author thinking_fioa 13/10/2019
+ */
+public class Leetcode66 {
+
+}
