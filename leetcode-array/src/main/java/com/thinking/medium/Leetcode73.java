045eb037be94c84806ed4f8cedc298a63db95444
thinking <thinking_fioa@163.com>
2019-10-27 21:22:21 +0800

    leetcode-array leetcode73

--- leetcode-array/src/main/java/com/thinking/medium/Leetcode73.java
+++ leetcode-array/src/main/java/com/thinking/medium/Leetcode73.java
@@ -9,9 +9,11 @@ package com.thinking.medium;
  *
  * O(m+n)基本思路：如果使用空间复杂度为O(m+n)，可以定义两个数组rows[m], column[n]，来标记某行、列是否存在元素'0'
  *
- * O(1)思路: 思路来源于O(m+n)，省略rows[m], column[n]，利用row[0](第一行)，column[0](第一列来代替)，用两个
+ * O(1)思路: 思路来源于O(m+n)，省略rows[m], column[n]，利用row[i](第i行)，column[j](第j列来代替)，用两个
  *
- * 变量先记录下row[0], column[0]，然后将队列中非第一行、第一列部分记录在row[0], column[0]即可
+ * 变量先记录下row[i], column[j]，然后将队列中非第i行、第j列部分记录在row[i], column[j]即可。
+ *
+ * 选择row[i]和column[j]要求是存在'0'元素
  *
  * 参考Go语言项目.
  *
