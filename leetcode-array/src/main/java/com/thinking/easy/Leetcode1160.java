40b9635d3ecdf6c712c5281f60c205c6cae00c1f
thinking <thinking_fioa@163.com>
2020-08-08 18:33:39 +0800

    leetcode 1160

---
+++ leetcode-array/src/main/java/com/thinking/easy/Leetcode1160.java
@@ -0,0 +1,43 @@
+package com.thinking.easy;
+
+import com.thinking.ConsoleOutput;
+
+/**
+ * @author thinking_fioa 2020/7/27
+ */
+public class Leetcode1160 {
+
+  public int countCharacters(String[] words, String chars) {
+    int count = 0;
+
+    for (int i = 0; i < words.length; i++) {
+      String str = words[i];
+      int j = 0;
+      String tempChars = chars;
+      for (; j < str.length(); j++) {
+        if (tempChars.contains(str.substring(j, j + 1))) {
+          tempChars = tempChars.replace(str.substring(j, j + 1), "");
+        } else {
+          break;
+        }
+      }
+      if (j == str.length()) {
+        System.out.println(str);
+        count += str.length();
+      }
+
+
+    }
+
+    return count;
+
+  }
+
+  public static void main(String[] args) {
+    String[] words = {"hello", "world", "leetcode"};
+    String chars = "welldonehoneyr";
+
+    Leetcode1160 l = new Leetcode1160();
+    ConsoleOutput.printf(l.countCharacters(words, chars));
+  }
+}
