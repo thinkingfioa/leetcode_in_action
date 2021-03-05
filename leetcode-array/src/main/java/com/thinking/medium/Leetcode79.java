3f03f53fc60c443e1df545b14214b4e392bbe54d
thinking <thinking_fioa@163.com>
2021-03-05 22:05:51 +0800

    Leetcode79 解题思路

--- leetcode-array/src/main/java/com/thinking/medium/Leetcode79.java
+++ leetcode-array/src/main/java/com/thinking/medium/Leetcode79.java
@@ -5,11 +5,16 @@ import com.thinking.ConsoleOutput;
 /**
  * Title: WordSearch
  * <p>
- * 题目:
+ * 题目: 给定一个二维数组board[][]，存放着小写和大写字母。再给定一个字符串word，
+ *
+ * 判断字符串word是否在二维数组board[][]中通过上下左右组装起来，每个元素只能使用一次
+ *
  * <p>
- * 相同题目：
+ * 相同题目：212 Word Search II
  * <p>
- * 思路：
+ * 思路：使用DFS搜索算法，最外层循环判断二维数组board[][]每个元素是否与word[0][0]相同，
+ *
+ * 如果相同，则开始DFS分别从上下左右查找。
  *
  * @author thinking_fioa 2021/3/3
  */
