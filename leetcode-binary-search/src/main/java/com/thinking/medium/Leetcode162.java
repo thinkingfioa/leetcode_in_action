f0945bce2071c861f289a66233e31d2129c8d50c
thinking <thinking_fioa@163.com>
2021-03-08 21:28:27 +0800

    Leetcode162 添加分析意见

--- leetcode-binary-search/src/main/java/com/thinking/medium/Leetcode162.java
+++ leetcode-binary-search/src/main/java/com/thinking/medium/Leetcode162.java
@@ -3,13 +3,14 @@ package com.thinking.medium;
 import com.thinking.ConsoleOutput;
 
 /**
- * Title:
+ * Title: Find Peak Element
  * <p>
- * 题目:
+ * 题目: 给定一个数组nums[]，在数组中找到一个元素 nums[i-1] < nums[i] > nums[i+1]，我们认为nums[i]为peak的元素。 同时定义nums[-1] =
+ * nums[n] = -∞。
  * <p>
- * 思路1:
+ * 思路1: 最简单的方法是，遍历一遍，每个元素判断一次。时间复杂度是O(n)
  * <p>
- * 思路2:
+ * 思路2: 采用二分查找方法，当left=mid时，判断是否当前的nums[mid]符合要求，否则二分查找[left, mid]进入左分支，出来后再[mid, right]进入右分支查找
  * <p>
  * 类似题型: 852 Peak Index in a Mountain Array
  *
@@ -47,7 +48,6 @@ public class Leetcode162 {
   }
 
   public static void main(String[] args) {
-//    int[] nums = {1, 2, 3, 1};
     int[] nums = {1, 2, 1, 3, 5, 6, 4};
     Leetcode162 leetcode162 = new Leetcode162();
     ConsoleOutput.printf(leetcode162.findPeakElement(nums));
