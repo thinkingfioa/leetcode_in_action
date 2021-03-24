4b7ff2d4ea20b69a70f74a6a131585dbf948fa99
thinking <thinking_fioa@163.com>
2021-03-24 22:52:48 +0800

    Leetcode316 Remove Duplicate Letters, and keep directory order

---
+++ leetcode-greedy/src/main/java/com/thinking/greedy/medium/Leetcode316.java
@@ -0,0 +1,30 @@
+package com.thinking.greedy.medium;
+
+import com.thinking.ConsoleOutput;
+
+/**
+ * Title: Remove Duplicate Letters
+ * <p>
+ * 题目:
+ * <p>
+ * 思路：
+ * <p>
+ * 类似题型:
+ */
+public class Leetcode316 {
+
+  public String removeDuplicateLetters(String s) {
+
+  }
+
+  public static void main(String[] args) {
+    // Output "abc"
+//    String s = "bcabc";
+    // Output "acdb"
+    String s = "cbacdcbc";
+    // Output "adbc"
+//    String s = "cdadabcc";
+    Leetcode316 leetcode316 = new Leetcode316();
+    ConsoleOutput.printf(leetcode316.removeDuplicateLetters(s));
+  }
+}
