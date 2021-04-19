c85bec3c0e2be9a6241d5cc596456c29e2950918
thinking <thinking_fioa@163.com>
2021-04-19 22:20:13 +0800

    Leetcode80 Remove Duplicates from Sorted Array II 补充题目和思路

--- leetcode-two-pointers/src/main/java/com/thinking/two/pointers/medium/Leetcode80.java
+++ leetcode-two-pointers/src/main/java/com/thinking/two/pointers/medium/Leetcode80.java
@@ -5,11 +5,15 @@ import com.thinking.ConsoleOutput;
 /**
  * Title: Remove Duplicated form Sorted Array II
  * <p>
- * 题目:
+ * 题目: 给定一个已经排序的数组nums，去除掉多余的数字，每个数字最多保留两个。要求空间复杂度为O(1)
  * <p>
- * 思路1:
+ * 思路1(推荐): 由于最多可以保留两个相同的数字，使用双指针，第一个指针result=2开始，用于纪录结果的。第二指针pos来遍历数组
  * <p>
- * 思路2:
+ * 每次nums[pos] == nums[result-1] && nums[result-2]时，则nums[pos]表示可以丢弃。
+ * <p>
+ * 思路2:这个是我自己想的方法。思路是当数组nums判断nums[i]==nums[i+1]时，说明可能有需要丢弃的数字，通过while循环
+ * <p>
+ * 往后续遍历，将后续相同的数字丢弃。
  * <p>
  * 类似题型: Median of Two Sorted Arrays
  * <p>
@@ -21,7 +25,25 @@ import com.thinking.ConsoleOutput;
  */
 public class Leetcode80 {
 
+  /**
+   * 推荐解法。
+   */
   public int removeDuplicates(int[] nums) {
+    if (nums.length <= 2) {
+      return nums.length;
+    }
+    int result = 2;
+    for (int i = 2; i < nums.length; i++) {
+      if (nums[i] == nums[result - 1] && nums[result - 1] == nums[result - 2]) {
+        continue;
+      } else {
+        nums[result++] = nums[i];
+      }
+    }
+    return result;
+  }
+
+  public int removeDuplicates2(int[] nums) {
     if (nums.length <= 2) {
       return nums.length;
     }
@@ -44,16 +66,11 @@ public class Leetcode80 {
     return result;
   }
 
-  public int removeDuplicates2(int[] nums) {
-    // TODO 思路2 by thinking_fioa
-    return -1;
-  }
-
   public static void main(String[] args) {
     // Output 5, nums=[1, 1, 2, 2, 3]
-//    int[] nums = {1, 1, 1, 2, 2, 3};
+    int[] nums = {1, 1, 1, 2, 2, 3};
     // Output 7 nums=[0, 0, 1, 1, 2, 3, 3]
-    int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
+//    int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
     Leetcode80 leetcode80 = new Leetcode80();
     ConsoleOutput.printf(leetcode80.removeDuplicates(nums));
     ConsoleOutput.printf(nums);
