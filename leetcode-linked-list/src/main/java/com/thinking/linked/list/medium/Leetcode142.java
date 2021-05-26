b6efcfb4961794052891171130864f03b25f0a34
thinking <thinking_fioa@163.com>
2021-05-26 22:27:19 +0800

    Leetcode142 Linked List Cycle II

---
+++ leetcode-linked-list/src/main/java/com/thinking/linked/list/medium/Leetcode142.java
@@ -0,0 +1,32 @@
+package com.thinking.linked.list.medium;
+
+/**
+ * Title: Linked List Cycle II
+ * <p>
+ * 题目:
+ * <p>
+ * 思路:
+ * <p>
+ * 方法一：利用HashMap存放已经遍历过的节点
+ * <p>
+ * 类似题型:
+ *
+ * @author thinking_fioa 2021/5/26
+ */
+public class Leetcode142 {
+
+  public ListNode detectCycle(ListNode head) {
+    return null;
+  }
+
+  class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+      val = x;
+      next = null;
+    }
+  }
+}
