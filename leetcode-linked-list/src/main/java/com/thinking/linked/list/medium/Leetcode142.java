b65b46a46cfb8da1d18ab6386d7154e93da28cc9
thinking <thinking_fioa@163.com>
2021-05-29 14:59:25 +0800

    Leetcode142 Linked List Cycle II

--- leetcode-linked-list/src/main/java/com/thinking/linked/list/medium/Leetcode142.java
+++ leetcode-linked-list/src/main/java/com/thinking/linked/list/medium/Leetcode142.java
@@ -7,16 +7,40 @@ package com.thinking.linked.list.medium;
  * <p>
  * 思路:
  * <p>
- * 方法一：利用HashMap存放已经遍历过的节点
+ * 方法一：快慢指针，fast指针一次走2步，slow指针一次走1步。
  * <p>
- * 类似题型:
+ * 方法二：利用HashMap存放已经遍历过的节点。如果题目要求是使用O(1)空间复杂度，则不适用
+ * <p>
+ * 类似题型: Linked List Cycle
+ * <p>
+ * Find the Duplicate Number
  *
  * @author thinking_fioa 2021/5/26
  */
 public class Leetcode142 {
 
+  /**
+   * fast 100.00%，less than 92.83%
+   */
   public ListNode detectCycle(ListNode head) {
-    return null;
+    ListNode fast = head;
+    ListNode slow = head;
+    while (true) {
+      if (slow == null || fast == null || fast.next == null) {
+        return null;
+      }
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) {
+        break;
+      }
+    }
+    ListNode other = head;
+    while (other != slow) {
+      other = other.next;
+      slow = slow.next;
+    }
+    return other;
   }
 
   class ListNode {
