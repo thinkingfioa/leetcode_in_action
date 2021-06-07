6e9989f817b2ebe08067355e89247522ba5ed42b
thinking <thinking_fioa@163.com>
2021-06-07 22:06:36 +0800

    Leetcode42 Trapping Rain Water 题目和解题思路

--- leetcode-array/src/main/java/com/thinking/array/hard/Leetcode42.java
+++ leetcode-array/src/main/java/com/thinking/array/hard/Leetcode42.java
@@ -5,9 +5,17 @@ import com.thinking.common.ConsoleOutput;
 /**
  * Title: Trapping Rain Water
  * <p>
- * 题目:
+ * 题目: 给定一个数字数组nums，每个元素都是正整数。nums[i]表示方块的高度，问：如果数组nums的所有方块按照顺序排列，下雨时
  * <p>
- * 思路:
+ * 能积累多少个方块雨水。
+ * <p>
+ * 思路: 其实题目本意是想求出凹进去的方块数目。假设遍历到nums[i]，我们不知道后面是否有nums[j]>nums[i](j >i)，
+ * <p>
+ * 如果有，那么数组nums在区间[i, j]之间就能积累雨水。
+ * <p>
+ * 我们比较好处理nums[i] < nums[j]的场景，试想我们分别从left -> right和right -> left计算依次，找到递增的两个数
+ * <p>
+ * 两个数之间就是能存的水滴数。
  * <p>
  * 类似题型: Product of Array Except Self
  * <p>
@@ -63,7 +71,7 @@ public class Leetcode42 {
 //    int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
     // 9
 //    int[] height = {4, 2, 0, 3, 2, 5};
-    //
+    // 2
     int[] height = {2, 0, 2};
     Leetcode42 leetcode42 = new Leetcode42();
     ConsoleOutput.printf(leetcode42.trap(height));
