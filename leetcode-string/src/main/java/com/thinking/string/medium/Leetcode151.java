60de36c4ea8217009bd01350d0f5084fd5fba084
thinking <thinking_fioa@163.com>
2021-07-04 13:58:06 +0800

    Leetcode151 Reverse Words in a String 解题思路

--- leetcode-string/src/main/java/com/thinking/string/medium/Leetcode151.java
+++ leetcode-string/src/main/java/com/thinking/string/medium/Leetcode151.java
@@ -5,11 +5,20 @@ import com.thinking.common.ConsoleOutput;
 /**
  * Title: Reverse Words in a String
  * <p>
- * 题目:
+ * 题目：给定一个字符串，要求将字符串中的所有单词逆转。比如给定一个s="blue is sky the"，输出结果为output="the sky is blue"
  * <p>
- * 思路
+ * 单词之间可能有一个或多个空格.
  * <p>
- * 类似题型:
+ * <p>
+ * 思路：题目要求不开辟空间，但是java语言中String是常量，意味着不可能。
+ * <p>
+ * 通过Java语言实现，题目比较简单，分为两个步骤：
+ * <p>
+ * 步骤1：将字符串中多个空格替换成单个空格
+ * <p>
+ * 步骤2：字符串按照空格切分，逆序遍历切分后的数组。
+ * <p>
+ * 类似题型: Reverse Words in a String II
  *
  * @author thinking_fioa 2021/6/5
  */
