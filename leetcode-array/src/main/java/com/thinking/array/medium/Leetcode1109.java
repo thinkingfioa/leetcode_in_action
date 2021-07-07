63c2be2a152f012c71d16c83d4908056e77f555d
thinking <thinking_fioa@163.com>
2021-07-07 21:54:40 +0800

    Leetcode1109 Corporate Flight Bookings 题目和题解

--- leetcode-array/src/main/java/com/thinking/array/medium/Leetcode1109.java
+++ leetcode-array/src/main/java/com/thinking/array/medium/Leetcode1109.java
@@ -5,9 +5,11 @@ import com.thinking.common.ConsoleOutput;
 /**
  * Title: Corporate Flight Bookings
  * <p>
- * 题目:
+ * 题目: 给一个二维数组bookings，每行有三个元素，分别是(fist, last, seat)代表航班[fist, last]之间每个航班买seat个座位。
  * <p>
- * 思路:
+ * 求：每个飞机航班共卖出总座位数目
+ * <p>
+ * 思路: 建立一个结果数组result[]，数组下标代表的是航班，result[i]表示第i个航班卖出的座位数。遍历二维数组bookings，依次填值。
  * <p>
  * 类似题型: H-Index II
  * <p>
