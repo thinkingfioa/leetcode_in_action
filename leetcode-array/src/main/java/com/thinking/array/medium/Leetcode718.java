9a56a9dea3697811a4a91bca52bd42b7d4adc60e
thinking <thinking_fioa@163.com>
2021-07-11 21:58:56 +0800

    Leetcode718 Maximum Length of Repeated Subarray 题目和题解

--- leetcode-array/src/main/java/com/thinking/array/medium/Leetcode718.java
+++ leetcode-array/src/main/java/com/thinking/array/medium/Leetcode718.java
@@ -5,11 +5,19 @@ import com.thinking.common.ConsoleOutput;
 /**
  * Title: Maximum Length of Repeated Subarray
  * <p>
- * 题目:
+ * 题目: 给定两个数组nums1[]和nums2[]，求两个数组中最长相同的字数组
  * <p>
- * 思路1: 滑动窗口。
+ * 思路1: 滑动窗口。求两个数组nums1[]和nums2[]的最长字数组，
  * <p>
- * 思路2: 动态规划
+ * 如果我们想下，以nums1[]为基础，遍历nums1[]中的每个元素，去nums2[]中找，如果相等就依次遍历最长的个数。这种算法能解决
+ * <p>
+ * nums1[]={1, 0, 0, 0}和nums2[]={0, 0, 0, 1}的场景，但是解决不了nums1[0, 0, 0, 1}和nums2[]={1, 0, 0, 0}场景
+ * <p>
+ * 所以可以两次交换获取最大即可。
+ * <p>
+ * 思路2: 动态规划。定义动态方程dp[i][j]表示nums1[i:]和nums2[j:]两个子数组的相同前缀的长度。
+ * <p>
+ * dp[i][j]=0(当num1[i]!=num[j])，dp[i][j]=dp[i+1][j+1]+1(当num1[i]==num[j])
  * <p>
  * 类似题型: Minimum Size Subarray Sum
  *
