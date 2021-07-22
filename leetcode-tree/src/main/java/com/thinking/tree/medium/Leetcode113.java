38f96be763d587a803706474c2440fdd6b4311b8
thinking <thinking_fioa@163.com>
2021-07-22 23:20:04 +0800

    Leetcode113 Path Sum II

---
+++ leetcode-tree/src/main/java/com/thinking/tree/medium/Leetcode113.java
@@ -0,0 +1,72 @@
+package com.thinking.tree.medium;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Title: Path Sum II
+ * <p>
+ * 题目:
+ * <p>
+ * 思路:
+ * <p>
+ * 类似题型: Path Sum
+ * <p>
+ * Binary Tree Paths
+ * <p>
+ * Path Sum III
+ * <p>
+ * Path Sum IV
+ *
+ * @author thinking_fioa 2021/7/21
+ */
+public class Leetcode113 {
+
+  /**
+   * faster than 99.98%, less than 39.64%
+   */
+  public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> single = new ArrayList<>();
+    preOrder(result, single, root, targetSum);
+    return result;
+  }
+
+  private void preOrder(List<List<Integer>> result, List<Integer> single, TreeNode t,
+      int targetSum) {
+    if (t == null) {
+      return;
+    }
+    if (t.left == null && t.right == null && targetSum == t.val) {
+      List<Integer> copy = new ArrayList<>(single);
+      copy.add(t.val);
+      result.add(copy);
+      return;
+    }
+    single.add(t.val);
+    preOrder(result, single, t.left, targetSum - t.val);
+    preOrder(result, single, t.right, targetSum - t.val);
+    single.remove(single.size() - 1);
+  }
+
+  public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
