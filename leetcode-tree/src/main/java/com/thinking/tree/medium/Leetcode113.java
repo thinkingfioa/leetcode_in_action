6c5b8df42c83c1b40d23050dbe76494affa7d3cd
thinking <thinking_fioa@163.com>
2021-07-25 19:29:25 +0800

    Leetcode113 Path Sum II 题目和题解

--- leetcode-tree/src/main/java/com/thinking/tree/medium/Leetcode113.java
+++ leetcode-tree/src/main/java/com/thinking/tree/medium/Leetcode113.java
@@ -7,9 +7,11 @@ import java.util.List;
 /**
  * Title: Path Sum II
  * <p>
- * 题目:
+ * 题目: 给定一个二叉树root，和一个数组targetSum。求出二叉树中从根节点到叶子节点相加和等于targetSum。
  * <p>
- * 思路:
+ * 思路: 利用先序遍历递归实现的思路，访问当前节点入栈，并执行targetSum'-p.val。递归结束后，移除。
+ * <p>
+ * 递归结束条件是: t->left == null && t->right == null && targetSum'==p.val
  * <p>
  * 类似题型: Path Sum
  * <p>
