f51a7856e15c622dc1fff9925e84937771365622
thinking <thinking_fioa@163.com>
2021-07-29 22:16:37 +0800

    Leetcode470 Implement Rand10() Using Rand7()

--- leetcode-other/src/main/java/com/thinking/other/medium/Leetcode470.java
+++ leetcode-other/src/main/java/com/thinking/other/medium/Leetcode470.java
@@ -3,9 +3,11 @@ package com.thinking.other.medium;
 /**
  * Title: Implement Rand10() Using Rand7()
  * <p>
- * 题目:
+ * 题目: 给定一个方法Rand7()，可以生成一个1-7之间的随机数。让基于Rand7()方法，实现Rand10()方法
  * <p>
- * 思路:
+ * 思路: Rand7()可以生成1-7之间的随机数，那么两次调用Rand7()可以产生49种可能。49种可能不能被10平均分，
+ * <p>
+ * 所以只取前40种可能映射到1-10之间。
  * <p>
  * 类似题型: Count Primes
  * <p>
@@ -25,7 +27,7 @@ public class Leetcode470 {
     do {
       int a = rand7();
       int b = rand7();
-      rand10 = (a - 1) * 7 + b - 1;
+      rand10 = (a - 1) * 7 + b;
     } while (rand10 > 40);
     return rand10 % 10 + 1;
   }
