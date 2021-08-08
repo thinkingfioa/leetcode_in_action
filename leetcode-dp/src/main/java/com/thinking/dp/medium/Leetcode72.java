72868acd487a48057cdf07850eb8fc76df0bf6d6
thinking <thinking_fioa@163.com>
2021-08-08 22:50:03 +0800

    Leetcode72 Edit Distance 题解

--- leetcode-dp/src/main/java/com/thinking/dp/medium/Leetcode72.java
+++ leetcode-dp/src/main/java/com/thinking/dp/medium/Leetcode72.java
@@ -6,9 +6,15 @@ import com.thinking.common.ConsoleOutput;
 /**
  * Title: Edit Distance
  * <p>
- * 题目：
+ * 题目：给定两个字符串word1和word2。要求word1通过增加、删除和替换等操作得到word2。求最少的操作数
  * <p>
- * 思路：
+ * 思路：两个字符串之间的处理比较，都可以通过动态规划实现。因为动态规划可以将前面比较的结果保存起来
+ * <p>
+ * 定义: dp[i][j]表示表示的是word1[0, i)通过这么多步骤操作可以得到word2[0, j)
+ * <p>
+ * 1. 当word1[i-1] == word2[j-1]时，dp[i][j] = dp[i-1][j-1]。无需操作
+ * <p>
+ * 2. 当word1[i-1] != word2[j-1]时，dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) +1。
  * <p>
  * 类似题型: One Edit Distance
  * <p>
