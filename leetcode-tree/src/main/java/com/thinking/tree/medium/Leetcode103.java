db003a0daf418d6e80d28b69450f84c88bbd2eaa
thinking <thinking_fioa@163.com>
2021-10-15 21:02:28 +0800

    Leetcode103 Binary Tree Zigzag Level Order Traversal

---
+++ leetcode-tree/src/main/java/com/thinking/tree/medium/Leetcode103.java
@@ -0,0 +1,96 @@
+package com.thinking.tree.medium;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * Title: Binary Tree Zigzag Level Order Traversal
+ * <p>
+ * 题目: 给定一个二叉树，按照Zigzag顺序层次遍历二叉树。顺序如下：
+ *
+ * <pre>
+ *                3                  ->   3
+ *         9             20          ->   20, 9
+ *      n     n     15      7        ->   15, 7
+ * </pre>
+ * <p>
+ * 思路:
+ * <p>
+ * 类似题型: Populating Next Right Pointers in Each Node II
+ * <p>
+ * Average of Levels in Binary Tree
+ * <p>
+ * Lowest Common Ancestor of Deepest Leaves
+ *
+ * @author thinking_fioa 2021/10/15
+ */
+public class Leetcode103 {
+
+  /**
+   * faster than 80.85%, less than 69.16%
+   *
+   * @param root
+   * @return
+   */
+  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    if (root == null) {
+      return new ArrayList<>();
+    }
+
+    List<List<Integer>> result = new ArrayList<>();
+    Deque<TreeNode> deque = new ArrayDeque<>();
+    deque.add(root);
+    boolean leftToRight = true;
+    while (!deque.isEmpty()) {
+      List<Integer> single = new ArrayList<>();
+      Deque<TreeNode> next = new ArrayDeque<>();
+      while (!deque.isEmpty()) {
+        TreeNode n;
+        if (leftToRight) {
+          n = deque.pollLast();
+          if (n.left != null) {
+            next.add(n.left);
+          }
+          if (n.right != null) {
+            next.add(n.right);
+          }
+        } else {
+          n = deque.pollLast();
+          if (n.right != null) {
+            next.add(n.right);
+          }
+          if (n.left != null) {
+            next.add(n.left);
+          }
+        }
+        single.add(n.val);
+      }
+      leftToRight = !leftToRight;
+      deque = next;
+      result.add(single);
+    }
+    return result;
+  }
+
+  public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
