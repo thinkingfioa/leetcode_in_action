c574b84701fc02387bbcdd561da50f88528c6ae8
thinking <thinking_fioa@163.com>
2021-10-19 22:53:03 +0800

    Leetcode103 Binary Tree Zigzag Level Order Traversal 题解

--- leetcode-tree/src/main/java/com/thinking/tree/medium/Leetcode103.java
+++ leetcode-tree/src/main/java/com/thinking/tree/medium/Leetcode103.java
@@ -16,7 +16,17 @@ import java.util.List;
  *      n     n     15      7        ->   15, 7
  * </pre>
  * <p>
- * 思路:
+ * 思路: 使用队列的层次遍历，由于题目需要以行输出，所以建议使用两个队列，一个是当前正在访问的队列，一个是给下次需要遍历的队列
+ * <p>
+ * 队列的遍历方式一定是从尾部先遍历，即后放入的先遍历，这样才能保证顺序性。
+ *
+ * 往下次遍历的队列中添加顺序要注意：
+ * <p>
+ * 定义一个leftToRight的变量，初始值为true，代表的是从左往右的遍历。
+ * <p>
+ * 如果leftToRight=true，那么访问当前队列的元素，添加到下次队列中的元素是先左节点，再右节点。
+ * <p>
+ * 如果leftToRight=false，那么访问当前队列的元素，添加到下次队列中的元素先右节点，再左节点。
  * <p>
  * 类似题型: Populating Next Right Pointers in Each Node II
  * <p>
@@ -30,9 +40,6 @@ public class Leetcode103 {
 
   /**
    * faster than 80.85%, less than 69.16%
-   *
-   * @param root
-   * @return
    */
   public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
     if (root == null) {
