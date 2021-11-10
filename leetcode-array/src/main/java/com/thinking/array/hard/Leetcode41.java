dcaafe26379983f7b457c8def609c89b34c17dd0
thinking <thinking_fioa@163.com>
2021-11-10 22:04:36 +0800

    Leetcode41 题目

--- leetcode-array/src/main/java/com/thinking/array/hard/Leetcode41.java
+++ leetcode-array/src/main/java/com/thinking/array/hard/Leetcode41.java
@@ -1,11 +1,19 @@
 package com.thinking.array.hard;
 
+import com.thinking.common.ConsoleOutput;
+
 /**
- * Title:
+ * Title: First Missing Positive
  * <p>
- * 题目:
+ * 题目: 给定一个无序的数组nums[]，找到数组中第一个未出现的正整数
+ * <pre>
+ *   input     3,4,-1,1
+ *   output    2
+ * </pre>
  * <p>
  * 思路:
+ * <p>
+ * 思路2：比较简单的想法是，先对数组排序，然后再找第一个未出现的正整数。但题目最终的要求是O(n)和常量的空间
  * <p>
  * 类似题型:
  *
@@ -13,7 +21,15 @@ package com.thinking.array.hard;
  */
 public class Leetcode41 {
 
-  public static void main(String[] args) {
+  public int firstMissingPositive(int[] nums) {
     
+  }
+
+  public static void main(String[] args) {
+    // Output 2
+    int[] nums = {3, 4, -1, 1};
+
+    Leetcode41 leetcode41 = new Leetcode41();
+    ConsoleOutput.printf(leetcode41.firstMissingPositive(nums));
   }
 }
