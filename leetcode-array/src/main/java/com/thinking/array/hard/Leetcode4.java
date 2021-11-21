cfe46ee36f5c3ba076da41fa8f5028a4baf79147
thinking <thinking_fioa@163.com>
2021-11-21 17:13:59 +0800

    Leetcode4 Median of Two Sorted Arrays 题解

--- leetcode-array/src/main/java/com/thinking/array/hard/Leetcode4.java
+++ leetcode-array/src/main/java/com/thinking/array/hard/Leetcode4.java
@@ -5,13 +5,27 @@ import com.thinking.common.ConsoleOutput;
 /**
  * Title: Median of Two Sorted Arrays
  * <p>
- * 题目:
+ * 题目: 给定两个有序的数组nums1[]和nums2[]，求两个有序数组的中位数。 难点：要求时间复杂度O(log(m+n))
+ * <pre>
+ *   输入: nums1 = [1,2], nums2 = [3,4]
+ *   输出: 2.50000
+ * </pre>
  * <p>
- * 思路1:
+ * 思路1: 题目的难点在于复杂度O(log(m+n))，对于两个有序的数组，求中位数，本质上可以转化为：
+ * <p>
+ * 求两个数组nums1[]和nums2[]的第(m+n+1)/2和第(m+n+2)/2的位置数。这个思路非常好，这样思考就无需要关心原数组的奇数和偶数的问题了。
+ * <p>
+ * 那么，如果求两个数组nums1[]和nums2[]的第K个元素呢？
+ * <p>
+ * 答：使用二分淘汰法。在两个数组nums1[]和nums2[]中找第K个元素，那么我们每次比较nums1[k/2]和nums2[k/2]，
+ * <p>
+ * 第一，nums1[k/2] < nums2[k/2]，那么nums1[begin, k/2]的全部被淘汰。
+ * <p>
+ * 第二，nums1[k/2] > nums2[k/2]，那么nums2[begin, k/2]的全部被淘汰。
  * <p>
  * 参考：https://www.nowcoder.com/discuss/196951
  * <p>
- * 思路2: 利用归并排序，新开辟一个数组，然后即可定位中位数
+ * 思路2: 利用归并排序，新开辟一个数组，然后即可定位中位数。该思路比较简单
  * <p>
  * 类似题型: Exclusive Time of Functions
  * <p>
