170151927f7a07e69f5af767139f00654966a1cf
thinking <thinking_fioa@163.com>
2021-12-26 21:58:00 +0800

    Leetcode394 Decode String

--- leetcode-string/src/main/java/com/thinking/string/medium/Leetcode394.java
+++ leetcode-string/src/main/java/com/thinking/string/medium/Leetcode394.java
@@ -7,11 +7,24 @@ import java.util.Deque;
 /**
  * Title: Decode String
  * <p>
- * 题目:
+ * 题目: 给定一个字符串，字符串中存在"k[encode_str]"类型的字串，其中k是一个数字型，encode_str字串，
+ * <p>
+ * "k[encode_str]"代表有k个字串encode_str组成，即：$encode_str$encode_str$encode_str...
+ * <p>
+ * 提醒：encode_str中也可以有"k[encode_str]"类型的字串。请还原出原串。
+ *
+ * <pre>
+ *   input :  3[a2[c]]
+ *   output： accaccacc
+ * </pre>
  * <p>
  * 思路:
  * <p>
- * 类似题型:
+ * 类似题型: Encode String with Shortest Length
+ * <p>
+ * Number of Atoms
+ * <p>
+ * Brace Expansion
  *
  * @author vlin 2021/12/22
  */
@@ -20,14 +33,58 @@ public class Leetcode394 {
   private static final Character LEFT_SQUARE = '[';
   private static final Character RIGHT_SQUARE = ']';
 
+  /**
+   * faster than 60.88%，less than 71.57%
+   */
   public String decodeString(String s) {
-    Deque<String> deque = new ArrayDeque();
+    if (s == null || s.length() <= 1) {
+      return s;
+    }
+    Deque<String> deque = new ArrayDeque<>();
 
-    
+    for (int i = 0; i < s.length(); i++) {
+      char single = s.charAt(i);
+      if (single == RIGHT_SQUARE) {
+        StringBuilder subSb = new StringBuilder();
+        while (!deque.isEmpty()) {
+          String top = deque.pollLast();
+          if ("[".equals(top)) {
+            String numTop = deque.pollLast();
+            String subStr = append(Integer.parseInt(numTop), subSb.toString());
+            deque.add(subStr);
+            break;
+          } else {
+            subSb.insert(0, top);
+          }
+        }
+
+      } else if (single >= '0' && single <= '9') {
+        int begin = i;
+        while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+          i++;
+        }
+        String subStr = s.substring(begin, i);
+        deque.add(subStr);
+        i = i - 1;
+      } else {
+        deque.add(String.valueOf(single));
+      }
+    }
+
+    StringBuilder result = new StringBuilder();
+    while (!deque.isEmpty()) {
+      result.append(deque.pollFirst());
+    }
+
+    return result.toString();
   }
 
-  private boolean isNum(Character c) {
-    return c >= '0' && c <= '9';
+  private String append(int count, String str) {
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < count; i++) {
+      result.append(str);
+    }
+    return result.toString();
   }
 
   public static void main(String[] args) {
