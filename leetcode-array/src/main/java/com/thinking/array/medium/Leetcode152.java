b5e6fe2e0be4fe32f1cdc9a36a0608c59d107355
thinking <thinking_fioa@163.com>
2022-02-28 22:49:37 +0800

    Leetcode152 Maximum Product Subarray 题解

--- leetcode-array/src/main/java/com/thinking/array/medium/Leetcode152.java
+++ leetcode-array/src/main/java/com/thinking/array/medium/Leetcode152.java
@@ -5,11 +5,22 @@ import com.thinking.common.ConsoleOutput;
 /**
  * Title: Maximum Product Subarray
  * <p>
- * 题目: 给定一个数组nums，数组中有负数和整数，求数组中子数组的乘积最大值
+ * 题目: 给定一个数组nums，数组中有负数、整数和0，求数组中子数组的乘积最大值
+ * <pre>
+ *     input:  {2, -3, 2, 4};
+ *     output: 8
+ *   </pre>
+ * 思路：子数组成乘积最大，假设我们先不考虑数值0，如果数组中所有元素为正数或偶数个负数，那么最大乘积就是所有元素
  * <p>
- * 思路：
+ * 如果数组元素存在奇数个负数，那么最大乘积要不是从第一个负数后一位开始所有元素组成最大乘积，要么是不包括最后一个负数后
  * <p>
- * 类似题型：
+ * 的所有元素组成的最大乘积
+ * <p>
+ * 类似题型：Product of Array Except Self
+ * <p>
+ * Maximum Product of Three Numbers
+ * <p>
+ * Subarray Product Less Than K
  *
  * @author vlin 2022/2/21
  */
@@ -49,10 +60,10 @@ public class Leetcode152 {
   }
 
   public static void main(String[] args) {
-    // Output 6
+    // Output 8
 //    int[] nums = {2, -3, 2, 4};
 
-    // Output 0
+    // Output 4
     int[] nums = {3, -1, 4};
 
     Leetcode152 leetcode152 = new Leetcode152();
