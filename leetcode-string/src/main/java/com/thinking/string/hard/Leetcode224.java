3f70726f43e4a56e09fc5f59e5df79b98e32d44a
thinking <thinking_fioa@163.com>
2022-03-05 22:13:19 +0800

    Leetcode224 Basic Calculator

--- leetcode-string/src/main/java/com/thinking/string/hard/Leetcode224.java
+++ leetcode-string/src/main/java/com/thinking/string/hard/Leetcode224.java
@@ -11,22 +11,56 @@ import java.util.Deque;
  * <p>
  * 思路：
  * <p>
- * 类似题型:
+ * 类似题型: Evaluate Reverse Polish Notation
+ * <p>
+ * Different Ways to Add Parentheses
+ * <p>
+ * Expression Add Operators
+ * <p>
+ * Basic Calculator III
+ * <p>
+ * The Score of Students Salving Math Expression
  *
  * @author vlin 2022/2/28
  */
 public class Leetcode224 {
 
-  private static final char SUB = '-';
-  private static final char PLUS = '+';
-  private static final char LEFT = '(';
-  private static final char RIGHT = ')';
-  private static final char EMPTY = ' ';
-
+  /**
+   * faster than 89.01%，less than 50.26%.
+   */
   public int calculate(String s) {
 
-    Deque<Integer> numQueue = new ArrayDeque<>();
+    Deque<Integer> queue = new ArrayDeque<>();
+    int sign = 1;
+    int result = 0;
+    int singleNum = 0;
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (c == ' ') {
+        continue;
+      }
+      if (c >= '0' && c <= '9') {
+        singleNum = singleNum * 10 + c - '0';
+      } else {
+        result = result + sign * singleNum;
+        singleNum = 0;
+        if (c == '+') {
+          sign = 1;
+        } else if (c == '-') {
+          sign = -1;
+        } else if (c == '(') {
+          queue.addLast(result);
+          queue.addLast(sign);
+          result = 0;
+          sign = 1;
+        } else if (c == ')') {
+          result = queue.pollLast() * result;
+          result += queue.pollLast();
+        }
+      }
+    }
 
+    return result + sign * singleNum;
   }
 
   public static void main(String[] args) {
@@ -35,6 +69,12 @@ public class Leetcode224 {
 
     // Output -12
 //    String s = "- (3+(4+5))";
+
+    // Output 11
+//    String s = "- (3-(4+5)) + 5";
+
+    // Output -15
+//    String s = "2-4-(8+2-6+(8+4-(1)+8-10))";
 
     Leetcode224 leetcode224 = new Leetcode224();
     ConsoleOutput.printf(leetcode224.calculate(s));
