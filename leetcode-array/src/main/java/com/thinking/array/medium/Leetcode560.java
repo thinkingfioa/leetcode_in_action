566f1fdee0a4ee04b1a0ac89767e49764bc8c39f
thinking <thinking_fioa@163.com>
2022-03-09 22:26:52 +0800

    Leetcode560 Subarray Sum Equals K

--- leetcode-array/src/main/java/com/thinking/array/medium/Leetcode560.java
+++ leetcode-array/src/main/java/com/thinking/array/medium/Leetcode560.java
@@ -1,24 +1,51 @@
 package com.thinking.array.medium;
 
 import com.thinking.common.ConsoleOutput;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Title: Subarray Sum Equals K
  * <p>
- * 题目:
+ * 题目: 给定一个数组nums[]和一个整数k，数组中既有正数也有负数，求数组nums[]的子数组的个数，该子数组的和等于k
  * <p>
  * 思路：
  * <p>
  * 思路2：动态规划dp[i][j]表示[i,j)的和，然后判断dp[i][j]==k，会出现Time Limit Exceeded
  * <p>
- * 类似题型：
+ * 类似题型：Continuous Subarray Sum
+ * <p>
+ * Subarray Product Less Than K
+ * <p>
+ * Find Pivot Index
+ * <p>
+ * Subarray Sums Divisible by K
+ * <p>
+ * Minimum Operations to Reduce X to Zero
+ * <p>
+ * K Radius Subarray Averages
  *
  * @author vlin 2022/3/5
  */
 public class Leetcode560 {
 
+  /**
+   * faster than 29.73%，less than 24.12%
+   */
   public int subarraySum(int[] nums, int k) {
-
+    int result = 0;
+    Map<Integer, Integer> mark = new HashMap<>();
+    mark.put(0, 1);
+    int sum = 0;
+    for (int num : nums) {
+      sum += num;
+      if (mark.containsKey(sum - k)) {
+        result += mark.get(sum - k);
+      }
+      mark.putIfAbsent(sum, 0);
+      mark.compute(sum, (key, value) -> value + 1);
+    }
+    return result;
   }
 
   /**
@@ -46,6 +73,10 @@ public class Leetcode560 {
     // Output 2
     int[] nums = {1, 1, 1};
     int k = 2;
+
+    // Output 2
+//    int[] nums = {1, 2, 3};
+//    int k = 3;
 
     Leetcode560 leetcode560 = new Leetcode560();
     ConsoleOutput.printf(leetcode560.subarraySum(nums, k));
