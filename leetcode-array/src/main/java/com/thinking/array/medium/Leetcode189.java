e2000f409949b1948c7391ae258aebda83cdf7d0
thinking <thinking_fioa@163.com>
2022-04-16 16:38:46 +0800

    Leetcode189 Rotate Array

---
+++ leetcode-array/src/main/java/com/thinking/array/medium/Leetcode189.java
@@ -0,0 +1,55 @@
+package com.thinking.array.medium;
+
+import com.thinking.common.ConsoleOutput;
+
+/**
+ * Title: Rotate Array
+ * <p>
+ * 题目：给一个数组nums和一个非负数字k，循转数组nums前k位
+ * <pre>
+ *   input:   nums = [1,2,3,4,5,6,7], k = 3
+ *   output:  [5,6,7,1,2,3,4]
+ * </pre>
+ * <p>
+ * 思路：
+ * <p>
+ * 类似题型：Reverse Words in a String II
+ *
+ * @author vlin 2022/3/27
+ */
+public class Leetcode189 {
+
+  /**
+   * faster than 81.72%，less than 90.39%
+   */
+  public void rotate(int[] nums, int k) {
+    if (k == 0) {
+      return;
+    }
+
+    k = k % nums.length;
+    swap(nums, 0, nums.length - k - 1);
+    swap(nums, nums.length - k, nums.length - 1);
+    swap(nums, 0, nums.length - 1);
+  }
+
+  private void swap(int[] nums, int begin, int end) {
+    while (begin < end) {
+      int tmp = nums[begin];
+      nums[begin] = nums[end];
+      nums[end] = tmp;
+      begin++;
+      end--;
+    }
+  }
+
+  public static void main(String[] args) {
+    // Output [5, 6, 7, 1, 2, 3, 4]
+    int[] nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+
+    Leetcode189 leetcode189 = new Leetcode189();
+    leetcode189.rotate(nums, k);
+    ConsoleOutput.printf(nums);
+  }
+}
